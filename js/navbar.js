8a401059ebffebad36ad580476deb0b9a003296e
seth-w-gilbert <seth.gilbert1@gmail.com>
2018-02-09 15:55:59 -0600

    started messing with Brython

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,8 @@
 var navHTML =  `<a href="index.html" id="title">Title</a>
-
                 <a id="reading" href="reading.html" class="link">Reading List</a>
                 <a id="resume" href="resume.html" class="link">Resume</a>
-                <a id="flipsy" href="flipsy.html" class="link">Flipsy</a>`;
+                <a id="flipsy" href="flipsy.html" class="link">Flips</a>
+                <a id="pyFlip" href="pyFlip.html" class="link">Flips2</a>`;
 
 var navbar = document.createElement('div');
 navbar.innerHTML = navHTML;
