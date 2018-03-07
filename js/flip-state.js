83621bb0b57a214a7bc2bf928538b498c9a1c91c
seth-w-gilbert <seth.gilbert1@gmail.com>
2018-03-06 22:10:01 -0600

    fixed and organized the child-rearing functions of flip-state.

--- js/flip-state.js
+++ js/flip-state.js
@@ -6,13 +6,13 @@ function FlipState(b){
     this.nextCol = 3;
     this.nextRow = 2;
     this.board = b; //input board representation.
-    this.isWhiteMove = true; //white just "played"
+    this.whitePlayed = true;
 
     this.createCopy = function(oldState){
         copy = new FlipState([]);
         copy.nextCol = oldState.nextCol;
         copy.nextRow = oldState.nextRow;
-        copy.isWhiteMove = oldState.isWhiteMove;
+        copy.whitePlayed = oldState.whitePlayed;
 
         //for each row in the board.
         oldState.board.forEach((a, i) => {
@@ -33,11 +33,12 @@ function FlipState(b){
         var child = this.createCopy(this);
 
         //switch whose move it is.
-        child.isWhiteMove = !this.isWhiteMove;
+        child.whitePlayed = !this.whitePlayed;
 
         //modify the child's board
         //place a piece at the next open spot
-        child.board[this.nextRow][this.nextCol] = child.isWhiteMove ? WHITE : BLACK;
+        child.play(this.nextCol, this.nextRow);
+        // child.board[this.nextRow][this.nextCol] = child.whitePlayed ? WHITE : BLACK;
 
         //set myself up to produce more feasible children.
         if(this.nextCol == 7){
@@ -83,6 +84,8 @@ function FlipState(b){
     }
 
     //return how good the computer is doing.
+    //Do I weight the edges and corner or trust that looking
+    //ahead will be enough?
     this.staticEvaluation = function(){
       var count = 0;
       this.board.forEach((row) => {
@@ -104,13 +107,11 @@ function FlipState(b){
       //TODO
     }
 
-    //returns true
-    //valid when not placed on top of another piece
+    //true when not placed on top of another piece
     //AND can make flips.
-    //TODO this is basically a copy of flipBetween from flipsy.js
-    //TODO either make it more versatile, or split up the work somehow.
+    //TODO should I cut out early?
     this.isValidMove = function(x, y){
-      var color = this.isWhiteMove ? WHITE : BLACK;
+      var color = this.whitePlayed ? BLACK : WHITE;
 
       //is the space unoccupied?
       var isValid = (this.board[y][x] === '');
@@ -119,129 +120,211 @@ function FlipState(b){
       var numFlipped = 0;
       if(isValid){
 
-        //check left
-        var xPos = x-1;
-        while(xPos > -1 && board[y][xPos] != color && this.board[y][xPos] != ''){
-            xPos--;
-        }
-        if(xPos != -1 && this.board[y][xPos] == color){
-          for(var i=(xPos+1); i < x; i++){
-              numFlipped++;
-          }
-        }
+        //count the flips.
+        numFlipped += this.leftFlips(x, y, color);
+        numFlipped += this.rightFlips(x, y, color);
+        numFlipped += this.upFlips(x, y, color);
+        numFlipped += this.downFlips(x, y, color);
 
-        //check right.
-        xPos = x+1;
-        while(xPos < 8 && this.board[y][xPos] != color && this.board[y][xPos] != ''){
-            xPos++;
-        }
-        if(xPos != 8 && (xPos > x+1) && this.board[y][xPos] == color){
-          for(var i = (xPos-1); i > x; i--){
-              numFlipped++;
-          }
-        }
-
-        //flip above
-        var yPos = y-1;
-        while(yPos > -1 && this.board[yPos][x] != color && this.board[yPos][x] != ''){
-            yPos--;
-        }
-        if(yPos != -1 && (yPos < y-1) && this.board[yPos][x] == color){
-          for(var i=(yPos+1); i < y; i++){
-              numFlipped++;
-          }
-        }
-
-        //flip below
-        yPos = y+1;
-        while((yPos < 8) && (this.board[yPos][x] != color) && (this.board[yPos][x] != '')){
-            yPos++;
-        }
-        if(yPos != 8 && board[yPos][x] == color){
-          for(var i=(yPos-1); i > y; i--){
-              numFlipped++;
-          }
-        }
-
-        //Diagonals
-        //flip up and left diagonal
-        xPos = x-1;
-        yPos = y-1;
-        while(xPos > -1 && yPos > -1 && this.board[yPos][xPos] != color && this.board[yPos][xPos] != ''){
-            xPos--;
-            yPos--;
-        }
-        if(yPos != -1 && xPos != -1 && this.board[yPos][xPos] == color){
-          xPos += 1;
-          yPos += 1;
-          while(yPos < y && xPos < x){
-              xPos += 1;
-              yPos += 1;
-              numFlipped++;
-          }
-        }
-
-        //flip up and right diagonal
-        xPos = x+1;
-        yPos = y-1;
-        while(xPos < 8 && yPos > -1 && this.board[yPos][xPos] != color && this.board[yPos][xPos] != ''){
-            xPos++;
-            yPos--;
-        }
-        if(yPos != -1 && xPos != 8 && this.board[yPos][xPos] == color){
-          xPos -= 1;
-          yPos += 1;
-          while(yPos < y && xPos > x){
-              numFlipped++;
-              xPos -= 1;
-              yPos += 1;
-          }
-        }
-
-        //flip down and left diagonal
-        xPos = x-1;
-        yPos = y+1;
-        while(xPos > -1 && yPos < 8 && this.board[yPos][xPos] != color && this.board[yPos][xPos] != ''){
-            xPos--;
-            yPos++;
-        }
-        if(yPos != 8 && xPos != -1 && this.board[yPos][xPos] == color){
-          xPos += 1;
-          yPos -= 1;
-          while(yPos > y && xPos < x){
-              numFlipped++;
-              xPos += 1;
-              yPos -= 1;
-          }
-        }
-
-        //flip down and right diagonal
-        xPos = x+1;
-        yPos = y+1;
-        while((xPos < 8 && yPos < 8) && this.board[yPos][xPos] != color && this.board[yPos][xPos] != ''){
-            xPos++;
-            yPos++;
-        }
-        if(yPos != 8 && xPos != 8 && this.board[yPos][xPos] == color){
-          xPos -= 1;
-          yPos -= 1;
-          while(yPos > y && xPos > x){
-              numFlipped++;
-              xPos -= 1;
-              yPos -= 1;
-          }
-        }
+        numFlipped += this.upLeftFlips(x,y,color);
+        numFlipped += this.upRightFlips(x,y,color);
+        numFlipped += this.downLeftFlips(x,y,color);
+        numFlipped += this.downRightFlips(x,y,color);
       }
 
-      //none of the short circuit checks fired, not valid.
       return numFlipped > 0;
+    }
+
+    this.play = function(x,y) {
+      color = this.whitePlayed ? WHITE : BLACK;
+      this.board[y][x] = color;
+
+      //do the flips
+      this.leftFlips(x, y, color, true);
+      this.rightFlips(x, y, color, true);
+      this.upFlips(x, y, color, true);
+      this.downFlips(x, y, color, true);
+
+      this.upLeftFlips(x,y,color, true);
+      this.upRightFlips(x,y,color, true);
+      this.downLeftFlips(x,y,color, true);
+      this.downRightFlips(x,y,color, true);
     }
 
     //edits the board after user clicks.
     this.placeUserMove = function(x, y){
       this.board[y][x] = BLACK;
-      this.isWhiteMove = false;
+      this.whitePlayed = false;
       //TODO edit the visuals? how far should I go?
+    }
+
+
+    //helper methods.
+    this.flipOne = function(x,y){
+      this.board[y][x] = (this.board[y][x] == WHITE ? BLACK : WHITE)
+    }
+
+    //inputs: (x,y), w/b, and a boolean
+    this.leftFlips = function(x, y, color, flip){
+      var numFlipped = 0;
+
+      var xPos = x-1;
+      while(xPos > -1 && board[y][xPos] != color && this.board[y][xPos] != ''){
+        xPos--;
+      }
+      if(xPos != -1 && this.board[y][xPos] == color){
+        for(var i=(xPos+1); i < x; i++){
+          if(flip){
+            this.flipOne(i, y);
+          }
+          numFlipped++;
+        }
+      }
+
+      return numFlipped;
+    }
+
+    this.rightFlips = function(x, y, color, flip){
+      var numFlipped = 0;
+      var xPos = x+1;
+      while(xPos < 8 && this.board[y][xPos] != color && this.board[y][xPos] != ''){
+        xPos++;
+      }
+      if(xPos != 8 && (xPos > x+1) && this.board[y][xPos] == color){
+        for(var i = (xPos-1); i > x; i--){
+          if(flip){
+            this.flipOne(i, y);
+          }
+          numFlipped++;
+        }
+      }
+      return numFlipped;
+    }
+
+    this.upFlips = function(x, y, color, flip){
+      var numFlipped = 0;
+      var yPos = y-1;
+      while(yPos > -1 && this.board[yPos][x] != color && this.board[yPos][x] != ''){
+        yPos--;
+      }
+      if(yPos != -1 && (yPos < y-1) && this.board[yPos][x] == color){
+        for(var i=(yPos+1); i < y; i++){
+          if(flip){
+            this.flipOne(x, i);
+          }
+          numFlipped++;
+        }
+      }
+      return numFlipped;
+    }
+
+    this.downFlips = function(x, y, color, flip){
+      var numFlipped = 0;
+      var yPos = y+1;
+      while((yPos < 8) && (this.board[yPos][x] != color) && (this.board[yPos][x] != '')){
+        yPos++;
+      }
+      if(yPos != 8 && board[yPos][x] == color){
+        for(var i=(yPos-1); i > y; i--){
+          if(flip){
+            this.flipOne(x, i);
+          }
+          numFlipped++;
+        }
+      }
+      return numFlipped;
+    }
+
+    this.upLeftFlips = function(x,y,color,flip){
+      var numFlipped = 0;
+      var xPos = x-1;
+      var yPos = y-1;
+      while(xPos > -1 && yPos > -1 && this.board[yPos][xPos] != color && this.board[yPos][xPos] != ''){
+        xPos--;
+        yPos--;
+      }
+      if(yPos != -1 && xPos != -1 && this.board[yPos][xPos] == color){
+        xPos += 1;
+        yPos += 1;
+        while(yPos < y && xPos < x){
+          if(flip){
+            this.flipOne(xPos,yPos);
+          }
+          xPos++;
+          yPos++;
+          numFlipped++;
+        }
+      }
+      return numFlipped;
+    }
+
+    this.upRightFlips = function(x,y,color,flip){
+      var numFlipped = 0;
+      var xPos = x+1;
+      var yPos = y-1;
+      while(xPos < 8 && yPos > -1 && this.board[yPos][xPos] != color && this.board[yPos][xPos] != ''){
+          xPos++;
+          yPos--;
+      }
+      if(yPos != -1 && xPos != 8 && this.board[yPos][xPos] == color){
+        xPos -= 1;
+        yPos += 1;
+        while(yPos < y && xPos > x){
+          if(flip){
+            this.flipOne(xPos, yPos);
+          }
+          numFlipped++;
+          xPos--;
+          yPos++;
+        }
+      }
+      return numFlipped;
+    }
+
+    this.downLeftFlips = function(x,y,color,flip){
+      var numFlipped = 0;
+      var xPos = x-1;
+      var yPos = y+1;
+      while(xPos > -1 && yPos < 8 && this.board[yPos][xPos] != color && this.board[yPos][xPos] != ''){
+        xPos--;
+        yPos++;
+      }
+      if(yPos != 8 && xPos != -1 && this.board[yPos][xPos] == color){
+        xPos += 1;
+        yPos -= 1;
+        while(yPos > y && xPos < x){
+          if(flip){
+            this.flipOne(xPos, yPos);
+          }
+          numFlipped++;
+          xPos++;
+          yPos--;
+        }
+      }
+      return numFlipped;
+    }
+
+    this.downRightFlips = function(x,y,color,flip){
+      var numFlipped = 0;
+      var xPos = x+1;
+      var yPos = y+1;
+      while((xPos < 8 && yPos < 8) && this.board[yPos][xPos] != color && this.board[yPos][xPos] != ''){
+        xPos++;
+        yPos++;
+      }
+      if(yPos != 8 && xPos != 8 && this.board[yPos][xPos] == color){
+        xPos -= 1;
+        yPos -= 1;
+        while(yPos > y && xPos > x){
+          if(flip){
+            this.flipOne(xPos, yPos);
+          }
+          numFlipped++;
+          xPos--;
+          yPos--;
+        }
+      }
+      return numFlipped;
     }
 }
 
@@ -258,10 +341,6 @@ var board =[['','','','','','','',''],
             ['','','','','','','','']];
 
 var state = new FlipState(board);
-/*console.log(state);
-console.log(state.staticEvaluation());
-console.log(state.isValidMove(3,2));
-console.log(state.isWhiteMove);*/
 
 var i = 0;
 while(state.hasMoreChildren()){
